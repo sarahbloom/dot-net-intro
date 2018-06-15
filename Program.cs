d60e20d03f82e47414bb7e435eb4ffc3f060b969
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-06-15 09:27:32 -0500

    more on list services

--- Program.cs
+++ Program.cs
@@ -8,9 +8,16 @@ namespace myApp
         static void Main(string[] args)
         {
         var names = new List<string> {"Alex", "Anna", "Felipe"};
-        foreach (var name in names)
+    
+            names.Add("Maria");
+            names.Add("Tony");
+            names.Remove("Alex");
+            foreach (var name in names)
             {
-            Console.WriteLine($"Hello {name.ToUpper()}!");
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+                Console.WriteLine($"Their name is {names[0]}");
+                Console.WriteLine($"I added {names[2]} and {names[3]} to the list!");
+                Console.WriteLine($"There are {names.Count} names in the list");
             }
         }
     }
