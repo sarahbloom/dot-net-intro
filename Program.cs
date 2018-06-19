8c85b5e3892326ba97f89e5a8f65ac173f9ad5f8
Sarah Bloomberg <sarah.l.bloomberg@gmail.com>
2018-06-18 21:46:05 -0500

    search and sort lists

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ namespace myApp
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main(string[] args){
+            WorkingWithStrings();
+        }
+        static void WorkingWithStrings()
         {
         var names = new List<string> {"Alex", "Anna", "Felipe"};
     
@@ -15,9 +19,35 @@ namespace myApp
             foreach (var name in names)
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
-                Console.WriteLine($"Their name is {names[0]}");
-                Console.WriteLine($"I added {names[2]} and {names[3]} to the list!");
-                Console.WriteLine($"There are {names.Count} names in the list");
+            }
+            Console.WriteLine($"Their name is {names[0]}");
+            Console.WriteLine($"I added {names[2]} and {names[3]} to the list!");
+            Console.WriteLine($"There are {names.Count} names in the list");
+
+            var index = names.IndexOf("Felipe");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+            }
+
+            index = names.IndexOf("Not Found");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            }
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
             }
         }
     }
